5adafbef7f8156a5b73c7c2d5ee66ca6cbec2ff8
ForHemer <foreste.hemerson@gmail.com>
2022-04-05 18:44:03 -0400

    Create basic structure for Missions

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Rockets from './components/rockets/Rocket';
+import Missions from './components/missions/Missions';
+import Profile from './components/MyProfile';
 
+const App = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Rockets />} />
+      <Route path="/missions" element={<Missions />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import logo from './assets/planet.png';
+import logo from '../assets/planet.png';
 
 const Navbar = () => (
 
