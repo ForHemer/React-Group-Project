be51e95431d7d51bdbccf2eb63eb71b09885b434
ForHemer <foreste.hemerson@gmail.com>
2022-04-06 17:46:33 -0400

    Create actions creators

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,1 +1,27 @@
-// empty for now;=
+const JOIN_MISSION = 'space-travelers/missions/JOIN_MISSION';
+const LEAVE_MISSION = 'space-travelers/missions/LEAVE_MISSION';
+const GET_MISSIONS = 'space-travelers/missions/GET_MISSIONS';
+const baseURL = 'https://api.spacexdata.com/v3/missions';
+
+// Action creators
+
+export const joinMission = (payload) => (
+  {
+    type: JOIN_MISSION,
+    payload,
+  }
+);
+
+export const leaveMission = (payload) => (
+  {
+    type: LEAVE_MISSION,
+    payload,
+  }
+);
+
+export const getMissions = (payload) => ({
+  type: GET_MISSIONS,
+  payload,
+
+});
+
