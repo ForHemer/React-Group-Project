db8cb16394c759ebf810fe6f0a61a31534233c03
Thi-Ponce <thi.pponce@gmail.com>
2022-04-06 15:32:25 -0300

    Add rocket reducer

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,1 +1,43 @@
-// empty for now
+const GETROCKET = 'rockets/rockets/GET_ROCKET';
+const RESERVEROCKET = 'rockets/rockets/RESERVE_ROCKET';
+const CANCELROCKET = 'rockets/rockets/CANCEL_ROCKET';
+
+export const rocketsData = (payload) => ({
+  type: GETROCKET,
+  payload,
+});
+
+export const reserveRocket = (id) => ({
+  type: RESERVEROCKET,
+  id,
+});
+
+export const removeReservation = (id) => ({
+  type: CANCELROCKET,
+  id,
+});
+
+const startingState = [];
+
+export const rocketReducer = (state = startingState, action) => {
+  switch (action.type) {
+    case GETROCKET:
+      return action.payload;
+    case RESERVEROCKET:
+      return state.map((rocket) => {
+        if (rocket.id !== action.id) {
+          return rocket;
+        }
+        return { ...rocket, reserved: true };
+      });
+    case CANCELROCKET:
+      return state.map((rocket) => {
+        if (rocket.id !== action.id) {
+          return rocket;
+        }
+        return { ...rocket, reserved: false };
+      });
+    default:
+      return state;
+  }
+};
