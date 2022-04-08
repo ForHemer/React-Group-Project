e4b1ba75aa268e81180111fa147241d343f44540
ForHemer <foreste.hemerson@gmail.com>
2022-04-08 15:03:45 -0400

    Create list of  all joined missions

--- src/components/profile/MyProfile.jsx
+++ src/components/profile/MyProfile.jsx
@@ -1,11 +1,16 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
+import Table from 'react-bootstrap/Table';
+import ProfileMissions from '../missions/ProfileMissions';
+import style from './Profile.module.css';
 
 const Profile = () => {
 // eslint-disable-next-line max-len
   const rockets = useSelector((state) => (state.rocketReducer || []).filter((rocket) => rocket.reserved === true));
-
+  const missionList = useSelector((state) => state.missionsReducer);
+  const profileMission = missionList.filter((mission) => mission.reserved === true);
   return (
-    <section>
+    <section className={style.flex}>
       <div className="my-rockets">
         <h2 className="rocket-title">
           My Rockets
@@ -14,6 +19,30 @@ const Profile = () => {
           {rockets.map((rocket) => (<li className="rocket-item" key={rocket.id}>{rocket.name}</li>))}
         </ul>
       </div>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th colSpan={2}>My Missions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {profileMission.length ? (
+            <>
+              {profileMission.map((item) => (
+                <ProfileMissions
+                  key={item.mission_id}
+                  id={item.mission_id}
+                  name={item.mission_name}
+                  description={item.mission_description}
+                />
+              ))}
+            </>
+          ) : (
+            <tr className="empty"><td>You have not joined any missions</td></tr>
+          )}
+        </tbody>
+      </Table>
     </section>
   );
 };
